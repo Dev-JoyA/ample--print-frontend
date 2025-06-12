a0e7aacb85f80d063cf0feb1085432ba474a6349
Joy A <joy.gold13@gmail.com>
2025-06-12 21:06:26 +0100

    logic for deactivating admin

--- src/app/admin-management/deactivate-admin/page.js
+++ src/app/admin-management/deactivate-admin/page.js
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client";
+
 
 const page = () => {
+    const handleClick = () => {
+        const [email, setEmail] = useState("");
+        const confirmation = confirm("Are you sure you want to deactivate this admin?");
+        if (confirmation) {
+            deactivateAdmin();
+        }
+        const response = fetch("http://localhost:4001/auth/deactivate-admin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email }) // Replace with actual admin ID
+        });
+        // Logic to deactivate admin
+        console.log("Admin deactivated");
+    }
   return (
     <div>Deactivate Admin Page</div>
   )
